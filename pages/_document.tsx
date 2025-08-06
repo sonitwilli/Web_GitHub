40f57b58b4a3f66ef1d79f9a09114e069d6c1b4f
lqhuy04 <128273460+lqhuy04@users.noreply.github.com>
2025-08-06 11:22:01 +0700

    handle safe area for mobile menu

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="vi">
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
